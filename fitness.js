4e450be007b91788088b6489a69a97bb9a5ebf8c
SGallacher87 <114994476+SGallacher87@users.noreply.github.com>
2023-01-10 19:12:55 +0000

    Add files via upload

---
+++ fitness.js
@@ -0,0 +1,39 @@
+const coach = document.getElementById ('coach');
+coach.addEventListener ('mouseover', function handleMouseOver() {
+    coach.style.color = 'coral';
+  });
+
+  coach.addEventListener('mouseout', function handleMouseOut() {
+    coach.style.color = 'black';
+  });
+
+
+
+  const res = document.getElementById ('res');
+res.addEventListener ('mouseover', function handleMouseOver() {
+    res.style.color = 'coral';
+  });
+
+  res.addEventListener('mouseout', function handleMouseOut() {
+    res.style.color = 'black';
+  });
+
+
+  const cust = document.getElementById ('cust');
+cust.addEventListener ('mouseover', function handleMouseOver() {
+    cust.style.color = 'coral';
+  });
+
+  cust.addEventListener('mouseout', function handleMouseOut() {
+    cust.style.color = 'black';
+  });
+
+  const btn = document.getElementById ('btn');
+  btn.addEventListener ('mouseover', function handleMouseOver() {
+      btn.style.color = 'white';
+    });
+  
+    btn.addEventListener('mouseout', function handleMouseOut() {
+      btn.style.color = 'black';
+    });
+
