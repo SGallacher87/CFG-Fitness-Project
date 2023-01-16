eab3b53e559fb78c05425f09a9084396409663d8
SGallacher87 <114994476+SGallacher87@users.noreply.github.com>
2023-01-16 19:24:27 +0000

    Add files via upload

--- fitness.js
+++ fitness.js
@@ -1,3 +1,17 @@
+const logo = document.getElementById ('logo');
+logo.addEventListener ('mouseover', function handleMouseOver() {
+    logo.style.color = 'white';
+  });
+
+  logo.addEventListener('mouseout', function handleMouseOut() {
+    logo.style.color = 'black';
+  });
+
+
+
+
+
+
 const coach = document.getElementById ('coach');
 coach.addEventListener ('mouseover', function handleMouseOver() {
     coach.style.color = 'coral';
@@ -31,9 +45,56 @@ cust.addEventListener ('mouseover', function handleMouseOver() {
   const btn = document.getElementById ('btn');
   btn.addEventListener ('mouseover', function handleMouseOver() {
       btn.style.color = 'white';
+
     });
   
     btn.addEventListener('mouseout', function handleMouseOut() {
       btn.style.color = 'black';
     });
+
+
+
+
+
+
+
+
+
+    const cfbtn = document.getElementById ('cf-button');
+  cfbtn.addEventListener ('mouseover', function handleMouseOver() {
+      cfbtn.style.color = 'white';
+
+    });
+  
+    cfbtn.addEventListener('mouseout', function handleMouseOut() {
+      cfbtn.style.color = 'black';
+    });
+
+
+    
+    
+    function myFunction() {
+        let person = alert("Congratulations you're a top athlete! Your top tips are on their way to you");
+        alert.style.color = 'coral';
+    }
+
+
+
+
+
+let mybutton = document.getElementById("go-top-button");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
 
